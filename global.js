9d1661d8ca5431c8a64d65d5433f08c79cfb0287
Tallal Khurram <i253023@isb.nu.edu.pk>
2025-12-01 22:13:00 +0500

    Added console log for contact us page

--- global.js
+++ global.js
@@ -1,3 +1,12 @@
 window.addEventListener("load", function () {
   console.log("Page loaded successfully");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var form = document.querySelector(".contact-form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // stops the page from reloading / actually submitting
+    alert("Your message has been submitted!");
+  });
+});
